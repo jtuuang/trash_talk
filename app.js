af2378329df4548795d26b941cdbc53f10da478e
jtuuang <jt.uuang@gmail.com>
2020-11-10 23:26:05 +0800

    Add helpers

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const bodyParser = require('body-parser')
 const generateTrashTalk = require('./generate_trash_talk')
 const port = 3000
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  helpers: {
+    ifEquals(job, selectedJob, options) {
+      return (job == selectedJob) ? options.fn(this) : options.inverse(this)
+    }
+  }
+}))
 app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -16,10 +23,7 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   const job = req.body.job
-  const eng = job === '工程師'
-  const des = job === '設計師'
-  const ent = job === '創業家'
-  res.render('index', { sentence: generateTrashTalk(job), eng, des, ent })
+  res.render('index', { sentence: generateTrashTalk(job), job })
 })
 
 app.listen(port, () => {
